5e4e5ca3165e626434d884d11b9343e49a3c0b4f
tomhealy1 <alfaone156@gmail.com>
2019-06-01 16:04:45 +0100

    Added updated DS

--- Employee_Attrition.py
+++ Employee_Attrition.py
@@ -1,5 +1,29 @@
 #Predicting Employee Attrition
 #01/06/2019
 
+#First lets Import the Pandas Module and do some analysis
 
+import pandas as pd 
+import numpy as np 
+
+#Next we will pass the csv file to variable hr
+hr = pd.read_csv('HR.csv')
+#We take the columns in the HR file and create a list called col_names
+col_names = hr.columns.tolist()
+print("Colunm Names:")
+print(col_names)
+print("\nSample data:")
+print(hr.head)
+
+#We dont need to update the names of the columns
+#Lets take a look at the data types in our dataset
+print(hr.dtypes)
+
+#Next lets check for missing values
+print(hr.isnull().any())
+
+#Looks good :+1:
+
+#Lets check the shape
+print(hr.shape)
 
