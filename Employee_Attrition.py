539ea40db6d6de3d1c9167b5742d7518fa8ff51e
tomhealy1 <alfaone156@gmail.com>
2019-06-03 20:03:31 +0100

    Added model evaluation, prec/recall and ROC curve

--- Employee_Attrition.py
+++ Employee_Attrition.py
@@ -13,6 +13,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.metrics import accuracy_score
+from sklearn import model_selection
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
 
 
 #Next we will pass the csv file to variable hr
@@ -98,12 +105,12 @@ for var in cat_var:
 hr.drop(hr.columns[[8,9]], axis=1, inplace=True)
 print(hr.columns.values)
 
-#Make a new list with the 
+#Make a new list with the values
 hr_vars=hr.columns.values.tolist()
 y=['left']
 X=[i for i in hr_vars if i not in y]
 
-
+#Time for Models - we are using Recursive Feature Elimination to do the hard work of feature selection n = 10
 model = LogisticRegression()
 rfe = RFE(model, 10)
 rfe = rfe.fit(hr[X], hr[y])
@@ -129,3 +136,50 @@ rf = RandomForestClassifier()
 rf.fit(X_train, y_train)
 
 print('Random Forest Accuracy: {:.3f}'.format(accuracy_score(y_test, rf.predict(X_test))))
+
+
+
+kfold = model_selection.KFold(n_splits=10, random_state=7)
+modelCV = RandomForestClassifier()
+scoring = 'accuracy'
+results = model_selection.cross_val_score(modelCV, X_train, y_train, cv=kfold, scoring=scoring)
+print("10-fold cross validation average accuracy: %.3f" % (results.mean()))
+
+
+print(classification_report(y_test, rf.predict(X_test)))
+
+
+y_pred = rf.predict(X_test)
+forest_cm = metrics.confusion_matrix(y_pred, y_test, [1,0])
+sns.heatmap(forest_cm, annot=True, fmt='.2f',xticklabels = ["Left", "Stayed"] , yticklabels = ["Left", "Stayed"] )
+plt.ylabel('True class')
+plt.xlabel('Predicted class')
+plt.title('Random Forest')
+plt.savefig('random_forest')
+plt.show()
+
+logit_roc_auc = roc_auc_score(y_test, logreg.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test)[:,1])
+
+rf_roc_auc = roc_auc_score(y_test, rf.predict(X_test))
+rf_fpr, rf_tpr, rf_thresholds = roc_curve(y_test, rf.predict_proba(X_test)[:,1])
+
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot(rf_fpr, rf_tpr, label='Random Forest (area = %0.2f)' % rf_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('ROC')
+plt.show()
+
+feature_labels = np.array(['satisfaction_level', 'last_evaluation', 'time_spend_company', 'Work_accident', 'promotion_last_5years', 
+      'department_RandD', 'department_hr', 'department_management', 'salary_high', 'salary_low'])
+importance = rf.feature_importances_
+feature_indexes_by_importance = importance.argsort()
+for index in feature_indexes_by_importance:
+    print('{}-{:.2f}%'.format(feature_labels[index], (importance[index] *100.0)))
